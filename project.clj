523e5a8036bf8bd2dbd4998a2b8800c140f70d77
Nic McPhee <mcphee@morris.umn.edu>
2016-05-02 16:28:23 -0500

    Renamed the project to match the new repo name.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hydro-evolution "0.1.0-SNAPSHOT"
+(defproject hydrofoil "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [expectations "2.1.8"]]
   :plugins [[lein-autoexpect "1.0"]]
-   :main hydrofoil.core)
+  :main hydrofoil.core)
