5eeae417f2a77028bccd450d98c6e4957e093013
AAaron The Dog <Dogxx000@bistromath.morris.umn.edu>
2016-05-08 15:26:55 -0500

    edited NACA Design

--- src/hydrofoil/core.clj
+++ src/hydrofoil/core.clj
@@ -9,7 +9,10 @@
   [max-camber position-camber thickness]
   (hash-map :corrected-max-camber (/ max-camber 100)
             :corrected-position-camber (/ position-camber 10)
-            :corrected-thickness (/ thickness 100)))
+            :corrected-thickness (/ thickness 100)
+            :max-camber max-camber
+            :positon-camber position-camber
+            :thickness thickness))
 
 
 (defn generate-random-design
