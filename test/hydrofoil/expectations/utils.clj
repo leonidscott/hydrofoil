376072f0181aca1deb9e7314f91c5221a4d7b265
Dogxx000 <scot0530@morris.umn.edu>
2017-03-07 22:29:54 -0600

    Add tests for Utils

--- test/hydrofoil/expectations/utils.clj
+++ test/hydrofoil/expectations/utils.clj
@@ -5,3 +5,5 @@
             [hydrofoil.evolution :refer :all]))
 
 ;;; --------- Utils Tests ------------
+(print "--------- Running Utils Tests ------------")
+(print "--------- Finished Utils Tests ------------")
