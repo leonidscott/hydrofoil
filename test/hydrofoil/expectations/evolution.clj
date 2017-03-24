284b6b578a35007f03d517d81557bd19b5b0f149
Dogxx000 <scot0530@morris.umn.edu>
2017-03-24 16:26:45 -0500

    Add evolution test clas

---
+++ test/hydrofoil/expectations/evolution.clj
@@ -0,0 +1,6 @@
+(ns hydrofoil.expectations.evolution
+  (:require [expectations :refer :all]
+            [hydrofoil.model :refer :all]
+            [hydrofoil.core :refer :all]
+            [hydrofoil.evolution :refer :all]
+            [hydrofoil.utils :refer :all]))
