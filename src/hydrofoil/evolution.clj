ed737669fe391e9e0ee593771a431e00112b2694
Dogxx000 <scot0530@morris.umn.edu>
2017-03-26 13:46:47 -0500

    Add p-min function, expand new-indiv to accomidate p-min function.
    p-min changes the lower value of p according to the value of thickness using a qudratic fit equation.
    This prevents undefined regions and snarls in the shape of the foil

--- src/hydrofoil/evolution.clj
+++ src/hydrofoil/evolution.clj
@@ -14,21 +14,45 @@
     lower-bound
     exploratory-limit))
 
+(defn p-min
+  [t]
+  (cond
+    (and (> t 0) (< t 20)) (+ 0.21934298928655427 (* 0.006602765526534473 (Math/pow t 2)))
+    ( = t 0) 0.0100
+    :else 2.79 ))
+
 (defn new-indv
   [parent]
   (let [exploration-factor 30 ;;; Doesn't do anything yet. hopefully will change m, p, and t, exp-factors.
         ;;; how far you can go from the parent value
         m-exp-factor 10
-        p-exp-factor 10
+        p-exp-factor 1
         t-exp-factor 10
         ;;; the numerical values of the upper limits
         m-upper-bound (upper-bound-check (+ m-exp-factor (parent :max-camber)) 100)
-        p-upper-bound (upper-bound-check (+ p-exp-factor (parent :positon-camber)) 100)
+        p-upper-bound (upper-bound-check (+ p-exp-factor (parent :positon-camber)) 10)
         t-upper-bound (upper-bound-check (+ t-exp-factor (parent :thickness)) 100)
         ;;; the numerical value of the lower limits
         m-lower-bound (lower-bound-check (- (parent :max-camber) m-exp-factor) 0)
-        p-lower-bound (lower-bound-check (- (parent :positon-camber) p-exp-factor) 0)
-        t-lower-bound (lower-bound-check (- (parent :thickness) t-exp-factor) 0)]
+        ;p-lower-bound (lower-bound-check (- (parent :positon-camber) p-exp-factor) 0)
+        t-lower-bound (lower-bound-check (- (parent :thickness) t-exp-factor) 0)
+
+        t-value (rand-double t-lower-bound t-upper-bound)
+        p-lower-bound (lower-bound-check (- (parent :positon-camber) p-exp-factor) (p-min t-value))]
+
     (NACA-design (rand-double m-lower-bound m-upper-bound)
                  (rand-double p-lower-bound p-upper-bound)
-                 (rand-double t-lower-bound t-upper-bound))))
+                 t-value)))
+
+
+(defn rand-individual
+  []
+  (let [t-value (rand-double 0 100)]
+    (NACA-design (rand-double 0 100)
+                 (rand-double (p-min t-value) 10)
+                 t-value)))
+
+
+
+
+
