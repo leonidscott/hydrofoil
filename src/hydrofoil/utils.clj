47467bd7d7ce1e8ad70a944c8311c27a09f098e1
morrislenny <scot0530@morris.umn.edu>
2018-06-07 15:48:21 -0400

    refactor round-double and rand-double

--- src/hydrofoil/utils.clj
+++ src/hydrofoil/utils.clj
@@ -2,19 +2,23 @@
 
 (defn NACA-design
   [max-camber position-camber thickness]
-  (hash-map :C-M (/ max-camber 100)
-            :C-P (/ position-camber 10)
-            :C-XX (/ thickness 100)
-            :M max-camber
-            :P position-camber
-            :XX thickness))
-
-(defn rand-double
-  "Takes a double and returns a double with four points of accuracy"
-  [lower-bound upper-bound]
-  (/ (double (+ (rand-int (* (- upper-bound lower-bound) 100)) (* lower-bound 100)))100))
+  {:C-M (/ max-camber 100)
+   :C-P (/ position-camber 10)
+   :C-XX (/ thickness 100)
+   :M max-camber
+   :P position-camber
+   :XX thickness})
 
 (defn round-double
    "Takes a double and returns a double with four points of accuracy"
    [n]
-   (double (/ (int (+ (* n 10000) 0.5)) 10000)))
+  (double (/ (int (+ (* n 10000)
+                     0.5))
+             10000)))
+
+(defn rand-double
+  [lower-bound upper-bound]
+  (let [diff (- upper-bound lower-bound)
+        r (rand diff)
+        r' (+ lower-bound r)]
+    (round-double r')))
